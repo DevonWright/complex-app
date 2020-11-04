6d0c6fcf52310db1d7dfb13422a3f25685dbd28c
DevonWright <devon.a.wright95@gmail.com>
2020-11-03 21:26:18 -0800

    controllers added

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = function() {
+
+}
+
+exports.logut = function() {
+    
+}
+
+exports.register = function() {
+    
+}
+
+exports.home = function(req, res) {
+    res.render('home-guest')
+}
